bc111bf2e0d0c23002243e6575fb68b128668509
virendraPrajapat18 <166205346+virendraPrajapat18@users.noreply.github.com>
2025-02-07 14:31:45 +0530

    Update User.jsx

--- Backend/Frontend/src/home/left/User.jsx
+++ Backend/Frontend/src/home/left/User.jsx
@@ -22,7 +22,7 @@ function User({user}) {
       <div className="flex space-x-4 px-8 py-7 hover:bg-slate-600 duration-300 cursor-pointer">
         <div className={`avatar ${ isOnline ? "avatar-online":""}`}>
           <div className="w-14 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            <img src="profile.png" />
           </div>
         </div>
 
